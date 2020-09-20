4f13805396110251dd3e8fb903084f7cf22f53ad
Josh Heng <josh@joshheng.co.uk>
2020-09-20 12:35:36 +0100

    Implement fetching school from code

--- Driver/firefly-api.js
+++ Driver/firefly-api.js
@@ -9,10 +9,38 @@ const xml = require('fast-xml-parser');
 class Firefly {
 	constructor(host, appId = 'FireFly Node.JS Driver') {
 		if (!host) throw 'Invalid host';
+
 		this.host = host;
 		this.appId = appId;
 	}
 
+	// Get a school from a code
+	static getHost(code) {
+		if (!code) throw 'Invalid code';
+
+		return new Promise(async (resolve, reject) => {
+			axios.get('https://appgateway.fireflysolutions.co.uk/appgateway/school/' + code)
+			.then(response => {
+				try {
+					response = xml.parse(response.data, { ignoreAttributes: false, allowBooleanAttributes: true })
+				}
+				catch (error) { return reject(error) }
+
+				if ((!response.response) || response.response['@_exists'] === 'false') return resolve(null);
+
+				return resolve({
+					enabled: response.response['@_enabled'] === 'true',
+					name: response.response.name,
+					id: response.response.installationId,
+					host: response.response.address['#text'],
+					ssl: response.response.address['@_ssl'] === 'true'
+				});
+			})
+			.catch(error => reject(error));
+		});
+	}
+
+	// Get the host api version
 	get apiVersion() {
 		return new Promise(async (resolve, reject) => {
 			axios.get(this.host + '/login/api/version')
@@ -26,7 +54,7 @@ class Firefly {
 					major: response.version.majorVersion,
 					minor: response.version.minorVersion,
 					increment: response.version.incrementVersion
-				})
+				});
 			})
 			.catch(error => reject(error));
 		});
--- Driver/test.js
+++ Driver/test.js
@@ -2,8 +2,13 @@ const Firefly = require('./firefly-api.js');
 require('dotenv').config();
 
 async function test() {
+	// Fetch school from code
+	console.log(await Firefly.getHost(process.env.CODE));
+
+	// Create instance
 	const instance = new Firefly(process.env.HOST);
 
+	// Test API version
 	console.log(await instance.apiVersion)
 }
 test();
